3a6b73e637007b9c289b1a9f236aeb76dfa68240
cosmonaut <evan@moonside.games>
2023-03-14 14:42:13 -0700

    add int clamp to MathHelper

--- src/Math/MathHelper.cs
+++ src/Math/MathHelper.cs
@@ -161,6 +161,26 @@ namespace MoonWorks.Math
 		}
 
 		/// <summary>
+		/// Restricts a value to be within a specified range.
+		/// </summary>
+		/// <param name="value">The value to clamp.</param>
+		/// <param name="min">
+		/// The minimum value. If <c>value</c> is less than <c>min</c>, <c>min</c>
+		/// will be returned.
+		/// </param>
+		/// <param name="max">
+		/// The maximum value. If <c>value</c> is greater than <c>max</c>, <c>max</c>
+		/// will be returned.
+		/// </param>
+		/// <returns>The clamped value.</returns>
+		public static int Clamp(int value, int min, int max)
+		{
+			value = (value > max) ? max : value;
+			value = (value < min) ? min : value;
+			return value;
+		}
+
+		/// <summary>
 		/// Calculates the absolute value of the difference of two values.
 		/// </summary>
 		/// <param name="value1">Source value.</param>
@@ -399,27 +419,6 @@ namespace MoonWorks.Math
 		#endregion
 
 		#region Internal Static Methods
-
-		// FIXME: This could be an extension! ClampIntEXT? -flibit
-		/// <summary>
-		/// Restricts a value to be within a specified range.
-		/// </summary>
-		/// <param name="value">The value to clamp.</param>
-		/// <param name="min">
-		/// The minimum value. If <c>value</c> is less than <c>min</c>, <c>min</c>
-		/// will be returned.
-		/// </param>
-		/// <param name="max">
-		/// The maximum value. If <c>value</c> is greater than <c>max</c>, <c>max</c>
-		/// will be returned.
-		/// </param>
-		/// <returns>The clamped value.</returns>
-		internal static int Clamp(int value, int min, int max)
-		{
-			value = (value > max) ? max : value;
-			value = (value < min) ? min : value;
-			return value;
-		}
 
 		internal static bool WithinEpsilon(float floatA, float floatB)
 		{
