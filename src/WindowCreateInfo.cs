cdc46671264cd7c76ffe5648b1824a7421e85a13
Evan Hemsley <2342303+thatcosmonaut@users.noreply.github.com>
2025-06-05 00:20:02 -0700

    High DPI Support (#21)

--- src/WindowCreateInfo.cs
+++ src/WindowCreateInfo.cs
@@ -29,6 +29,11 @@
 		/// Specifies if the window will open at the maximum desktop resolution.
 		/// </summary>
 		public bool StartMaximized;
+		/// <summary>
+		/// Specifies that a high pixel density window should be requested.
+		/// If the system does not support high DPI, this will have no effect.
+		/// </summary>
+		public bool HighDPI;
 
 		public WindowCreateInfo(
 			string windowTitle,
@@ -36,14 +41,17 @@
 			uint windowHeight,
 			ScreenMode screenMode,
 			bool systemResizable = false,
-			bool startMaximized = false
-		) {
+			bool startMaximized = false,
+			bool highDPI = false
+		)
+		{
 			WindowTitle = windowTitle;
 			WindowWidth = windowWidth;
 			WindowHeight = windowHeight;
 			ScreenMode = screenMode;
 			SystemResizable = systemResizable;
 			StartMaximized = startMaximized;
+			HighDPI = highDPI;
 		}
 	}
 }
